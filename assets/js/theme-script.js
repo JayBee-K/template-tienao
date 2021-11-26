9983a65d2d7a5c636f61ff1233bbea8cee613e18
Jay B <79182784+JayBee-K@users.noreply.github.com>
2021-11-26 10:33:08 +0700

    update

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -3,81 +3,6 @@ var heightSidebar = function () {
 	let heightHeaderSidebar = $('#changeHeightSidebar .sidebar-item .card .card-header').outerHeight(true);
 	$('#changeHeightSidebar .sidebar-item .card .card-body').css('max-height', heightSidebar - heightHeaderSidebar);
 }
-/*
-	Function thông báo
-	Toast bootstrap 4
-	Position có 4 class: top-0, left-0, right-0, bottom-0 (vị trí sẽ show thông báo)
-	Code mẫu nằm trong phần "Document ready - submit form"
- */
-var call_toastr = function (msg, type, time, position, parentElm) {
-	if (!position) {
-		position = "position-fixed top-0 right-0";
-	}
-	let classFind = position.replaceAll(" ", ".");
-	if (!time) {
-		time = 3000;
-	}
-	let html = "";
-	let hasContainer = ($(".toast-container." + classFind).length > 0);
-	let icon = "";
-	let bg = "";
-	let textColor = "";
-	if (type == "success") {
-		icon = '<i class="fal fa-check-circle"></i>';
-		bg = "bg-success bg-gradient";
-		textColor = "text-white";
-	} else if (type == "info") {
-		icon = '<i class="fal fa-info-circle"></i>';
-		bg = "bg-info bg-gradient";
-		textColor = "text-white";
-	} else if (type == "warning") {
-		icon = '<i class="fal fa-exclamation-triangle"></i>';
-		bg = "bg-warning bg-gradient";
-		textColor = "text-white";
-	} else if (type == "error") {
-		icon = '<i class="fal fa-times-circle"></i>';
-		bg = "bg-danger bg-gradient";
-		textColor = "text-white";
-	}
-	if (!hasContainer) {
-		html += `<div class="template-4_toast toast-container ${position}">`;
-	}
-	html += `<div class="toast ${bg} ${textColor}" role="alert" aria-live="assertive" data-delay="${time}"
-         aria-atomic="true">
-		<div class="toast-body d-flex justify-content-between">
-			<div class="toast-text d-flex">
-				 <div class="mr-2 toast-icon">
-					${icon}
-				 </div>
-				 ${msg}
-			</div>
-			<button type="button" class="mb-1 ml-2 close" data-dismiss="toast" aria-label="Close"><i class="fal fa-times"></i></button>
-		</div>
-    </div>`;
-	
-	let elmContainer;
-	if (!hasContainer) {
-		html += `</div>`;
-		if (parentElm) {
-			parentElm.append(html);
-		} else {
-			$("body").append(html);
-		}
-		elmContainer = $(".toast-container." + classFind);
-	} else {
-		elmContainer = $(".toast-container." + classFind);
-		elmContainer.append(html);
-	}
-	
-	elmContainer.find(".toast:not(.fade)")[0].addEventListener('hidden.bs.toast', function (e) {
-		e.target.remove();
-		if (elmContainer.find(".toast").length == 0) {
-			elmContainer.remove();
-		}
-	});
-	
-	elmContainer.find(".toast:not(.fade)").toast("show");
-};
 
 var formatPrice = function (n, currency) {
 	return n.toFixed(0).replace(/./g, function (c, i, a) {
@@ -154,15 +79,6 @@ $(document).ready(function () {
 		setTimeout(function () {
 			$('body').addClass('modal-open');
 		}, 500)
-	});
-	
-	$('.form-validated').submit(function (e) {
-		e.preventDefault();
-		if ($('.form-validated select').val() == -1) {
-			call_toastr("Vui lòng chọn ...", 'error', '5000');
-		} else {
-			call_toastr("Đây là thông báo thành công", 'success', '5000');
-		}
 	});
 	
 	$('.choose-bank').change(function () {
