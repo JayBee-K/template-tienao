14c9083139fb6530479773fb50c28c0042f64752
Jay <killerdrug007@gmail.com>
2023-04-08 18:52:24 +0700

    Update

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -1,3 +1,5 @@
+let windowWidth = $(window).width();
+
 var heightSidebar = function () {
 	let heightSidebar = $('#heightSidebar').outerHeight(true);
 	let heightHeaderSidebar = $('#changeHeightSidebar .sidebar-item .card .card-header').outerHeight(true);
@@ -49,8 +51,66 @@ const callMenu = function () {
 	}
 }
 
+let initSetHrefMenu = function () {
+	if ($("#template-4_header__v2 .header-navigation > ul > li > ul").length) {
+		$("#template-4_header__v2 .header-navigation > ul > li > ul").each(function (index) {
+			$(this).prev().attr({
+				'data-href': $(this).prev().attr('href'),
+			});
+		});
+	}
+}
+
+let initHeaderMobile = function () {
+	if (windowWidth < 1025) {
+		if ($("#template-4_header__v2 .header-navigation > ul > li > ul").length) {
+			$("#template-4_header__v2 .header-navigation").attr('id', 'hasMenu');
+			$("#template-4_header__v2 .header-navigation > ul > li > ul").each(function (index) {
+				$(this).prev().attr({
+					"href": "#subMenu_" + index,
+					"data-toggle": "collapse"
+				});
+
+				$(this).attr({
+					"id": "subMenu_" + index,
+					"class": "collapse list-unstyled mb-0 ",
+					"data-parent": "#hasMenu",
+				});
+			});
+		}
+
+		$('#call-navigation').click(function () {
+			$('body').toggleClass('is-navigation');
+		})
+
+		$('#template-4_header__v2-overlay').click(function () {
+			$("#template-4_header__v2 .header-navigation > ul > li > ul").collapse('hide');
+			$('body').removeClass('is-navigation is-users');
+		});
+
+		$('#call-users').click(function () {
+			$('body').addClass('is-users');
+		});
+	} else {
+		if ($("#template-4_header__v2 .header-navigation > ul > li > ul").length) {
+			$("#template-4_header__v2 .header-navigation").attr('id', '');
+			$("#template-4_header__v2 .header-navigation > ul > li > ul").each(function (index) {
+				$(this).prev().attr({
+					"href": $(this).prev().attr('data-href'),
+					"data-bs-toggle": ""
+				});
+
+				$(this).attr({
+					"id": "",
+					"class": "list-unstyled mb-0 position-absolute zi-2 bg-white d-flex flex-column transition-default border start-50 translate-middle-x",
+					"data-bs-parent": "",
+				});
+			});
+		}
+	}
+}
+
 $(document).ready(function () {
-	let windowWidth = $(window).width();
 	if (windowWidth < 992) {
 		$(".template-4_header .navigation-area > ul > li > ul").each(function (index) {
 			$(this).prev().attr({
@@ -64,15 +124,15 @@ $(document).ready(function () {
 			});
 		})
 	}
-	
+
 	$('#hamburger, .template-4_header > .overlay').click(function () {
 		callMenu();
 	});
-	
+
 	heightSidebar();
-	
+
 	$('[data-toggle="tooltip"]').tooltip()
-	
+
 	$(document).on("click", ".callModal", function () {
 		$('.modal').modal('hide');
 		$('#' + $(this).data('modal')).modal('show');
@@ -80,23 +140,33 @@ $(document).ready(function () {
 			$('body').addClass('modal-open');
 		}, 500)
 	});
-	
+
 	$('.choose-bank').change(function () {
 		if ($(this).val() != -1) {
 			$(this).parents('form').find('.name-bank').html($(this).find('option:selected').data('name'));
 		}
 	});
-	
+
 	$('.change-coin').keyup(function () {
 		let coin_convert = 0;
 		if ($(this).val() != '') {
 			coin_convert = parseInt($(this).val() * $(this).data('price'));
 		}
-		
+
 		$('.result-coin').val(formatPrice(coin_convert, '₫'));
 	});
-	
+
 	$('.updateInformation').click(function () {
 		fnUpdateInformation($(this).data('form'));
+	});
+
+
+	initSetHrefMenu();
+	initHeaderMobile();
+
+
+	$(window).resize(function () {
+		windowWidth = $(window).width();
+		initHeaderMobile();
 	});
 });
